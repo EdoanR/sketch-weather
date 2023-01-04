d8e50e963f49ea42515b0f6804863fe3b8b55534
Edoan <edoan.rodrigues@gmail.com>
2023-01-04 10:55:53 -0300

    passed functions and properties to entity.jsx

--- src/components/Entities/Car.jsx
+++ src/components/Entities/Car.jsx
@@ -1,9 +1,7 @@
-import { Component, createRef } from "react";
 import { ENTITIES_IDS, TEMP_TYPES } from "../../constants";
-import { randArray } from "../../utils";
-// import Entity from "./Entity";
+import Entity from "./Entity";
 
-export default class Car extends Component {
+export default class Car extends Entity {
     constructor(props) {
         super(props)
         
@@ -13,68 +11,11 @@ export default class Car extends Component {
             minTime: 5000,
             chancePerTick: 1
         }
-
-        this.lastSpawnTick = 0
-        this.maxSpawnTime = 10000
-
-        this.spawnChances = {}
-        this.updateSpawnChances()
-
-        this.lastDespawnTick = Date.now()
-
-        this.state = {
-            moving: false,
-            spriteClass: ''
-        }
-
-        this.element = createRef()
-
     }
 
-    onClick(e) {
-        this.despawn(true)
-    }
-
-    spawn() {
-        this.setState({ moving: true })
-        this.lastSpawnTick = Date.now()
-    }
-
-    despawn(addParticle = false) {
-
-        if (addParticle) this.spawnParticles()
-
-        this.setState({ moving: false })
-        this.lastDespawnTick = this.props.tick
-        this.updateSpawnChances()
-    }
-
-    componentDidUpdate(prevProps, prevState) {
-        if (prevProps.tick !== this.props.tick) this.handleTickChange(this.props.tick)
-
-        if (!this.props.weather || (prevProps.weather && prevProps.weather.id === this.props.weather.id)) return
-
-        this.handleDataChange(this.props.weather)
-    }
-
-    hasSpawnCondition(weather) {
+    isInSpawnCondition(weather) {
         if (weather.temp <= -10) return false
         return true
-    }
-
-    handleDataChange(weather) {
-
-        if (!this.hasSpawnCondition(weather)) {
-            if (this.state.moving) this.despawn(true)
-            return
-        }
-
-        const spriteClass = this.getSpriteClass(weather) || ''
-
-        // Spawn particles when entity change sprite on screen.
-        if (this.state.moving && spriteClass !== this.state.spriteClass) this.spawnParticles()
-        
-        this.setState({ spriteClass: spriteClass })
     }
 
     getSpriteClass({ tempType, temp }) {
@@ -86,72 +27,5 @@ export default class Car extends Component {
         } else {
             return 'car-normal';
         }
-    }
-
-    handleAnimationEnd(e) {
-        if (e.animationName === 'move') {
-
-            // Move animation fully completed.
-            this.despawn()
-        }
-    }
-
-    rollSpawnChance(tick = null) {
-        if (tick) {
-            const maxTick = this.spawnChances.maxTime == null ? null : this.lastDespawnTick + this.spawnChances.maxTime
-            const minTick = this.spawnChances.minTime == null ? null : this.lastDespawnTick + this.spawnChances.minTime
-
-            if (minTick && tick <= minTick) return false
-            if (maxTick && tick >= maxTick) return true
-        }
-
-        const chance = this.spawnChances.chancePerTick ?? 1
-        return Math.random() <= chance
-    }
-
-    handleTickChange(tick) {
-        if (!this.props.weather) return
-
-        if (!this.state.moving) {
-
-            if ( !this.hasSpawnCondition(this.props.weather) ) return
-            if ( !this.rollSpawnChance(tick) ) return
-
-            this.spawn()
-        } else {
-            if (tick >= this.lastSpawnTick + this.maxSpawnTime) 
-                this.despawn() // Took too long to despawn, maybe animation end was not triggered for some reason
-        }
-    }
-
-    composeClassName() {
-        let classes = [this.entityClass]
-
-        if (this.state.moving) classes.push('move')
-        if (this.state.spriteClass) classes.push(this.state.spriteClass)
-
-        return classes.join(' ')
-    }
-
-    updateSpawnChances() {
-        const propertiesToCheck = ['minTime', 'maxTime', 'chancePerTick'];
-
-        for (const prop of propertiesToCheck) {
-            if (this.spawnChancesConfig[prop] !== undefined) {
-                if (Array.isArray(this.spawnChancesConfig[prop])) {
-                    this.spawnChances[prop] = randArray(this.spawnChancesConfig[prop]);
-                } else {
-                    this.spawnChances[prop] = this.spawnChancesConfig[prop];
-                }
-            }
-        }
-    }
-
-    spawnParticles() {
-        this.props.particles.current.addParticle(this.element.current)
-    }
-
-    render() {
-        return <div ref={this.element} onAnimationEnd={(e) => this.handleAnimationEnd(e)} className={this.composeClassName()} onClick={(e) => this.onClick(e)} />
     }
 }
--- src/components/Entities/Entity.jsx
+++ src/components/Entities/Entity.jsx
@@ -1,38 +1,159 @@
-import { Component } from "react";
-import { CLASSES } from "../../constants";
+import { Component, createRef } from "react";
+import { randArray } from "../../utils";
+// import Entity from "./Entity";
 
-// Sprites are in the entity.scss
 export default class Entity extends Component {
-
     constructor(props) {
         super(props)
+        
+        this.entityClass = 'entity-class'
 
-        this.className = ''
+        this.spawnChancesConfig = {
+            minTime: 5000,
+            maxTime: 10000,
+            chancePerTick: 0.1
+        }
+
+        this.spawnChances = {}
 
         this.state = {
-            collectable: false,
-            collected: false
+            moving: false,
+            spriteClass: ''
+        }
+
+        this.lastSpawnTick = 0
+        this.maxSpawnTime = 10000
+        this.lastDespawnTick = Date.now()
+
+        this.element = createRef()
+
+    }
+    
+    componentDidMount() {
+        this.updateSpawnChances()
+    }
+
+    onClick(e) {
+        this.despawn(true)
+    }
+
+    spawn() {
+        this.setState({ moving: true })
+        this.lastSpawnTick = Date.now()
+    }
+
+    despawn(addParticle = false) {
+
+        if (addParticle) this.spawnParticles()
+
+        this.setState({ moving: false })
+        this.lastDespawnTick = this.props.tick
+        this.updateSpawnChances()
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        if (prevProps.tick !== this.props.tick) this.handleTickChange(this.props.tick)
+
+        if (!this.props.weather || (prevProps.weather && prevProps.weather.id === this.props.weather.id)) return
+
+        this.handleDataChange(this.props.weather)
+    }
+
+    isInSpawnCondition(weather) {
+        return true
+    }
+
+    handleDataChange(weather) {
+
+        if (!this.isInSpawnCondition(weather)) {
+            if (this.state.moving) this.despawn(true)
+            return
+        }
+
+        const spriteClass = this.getSpriteClass(weather) || ''
+
+        // Spawn particles when entity change sprite on screen.
+        if (this.state.moving && spriteClass !== this.state.spriteClass) this.spawnParticles()
+        
+        this.setState({ spriteClass: spriteClass })
+    }
+
+    getSpriteClass({ tempType, temp }) {
+        return ''
+    }
+
+    handleAnimationEnd(e) {
+        if (e.animationName === 'move') {
+
+            // Move animation fully completed.
+            this.despawn()
+        }
+    }
+
+    rollSpawnChance(tick = null) {
+        if (tick) {
+            const maxTick = this.spawnChances.maxTime == null ? null : this.lastDespawnTick + this.spawnChances.maxTime
+            const minTick = this.spawnChances.minTime == null ? null : this.lastDespawnTick + this.spawnChances.minTime
+
+            if (minTick && tick <= minTick) return false
+            if (maxTick && tick >= maxTick) return true
+        }
+
+        const chance = this.spawnChances.chancePerTick ?? 1
+        return Math.random() <= chance
+    }
+
+    handleTickChange(tick) {
+        if (!this.props.weather) return
+
+        if (!this.state.moving) {
+
+            if ( !this.isInSpawnCondition(this.props.weather) ) return
+            if ( !this.rollSpawnChance(tick) ) return
+
+            this.spawn()
+        } else {
+            if (tick >= this.lastSpawnTick + this.maxSpawnTime) 
+                this.despawn() // Took too long to despawn, maybe animation end was not triggered for some reason
         }
     }
 
     composeClassName() {
-        let className = this.className
+        let classes = [this.entityClass]
 
-        if (this.state.collectable) className += ' ' + CLASSES.collectable
-        if (this.state.collected) className += ' ' + CLASSES.collected
+        if (this.state.moving) classes.push('move')
+        if (this.state.spriteClass) classes.push(this.state.spriteClass)
 
-        return className
+        return classes.join(' ')
     }
 
-    handleClick(e) {
+    updateSpawnChances() {
+        const propertiesToCheck = ['minTime', 'maxTime', 'chancePerTick'];
 
-        if (!this.state.collectable || this.state.collected) return
+        for (const prop of propertiesToCheck) {
+            if (this.spawnChancesConfig[prop] !== undefined) {
+                if (Array.isArray(this.spawnChancesConfig[prop])) {
+                    this.spawnChances[prop] = randArray(this.spawnChancesConfig[prop]);
+                } else {
+                    this.spawnChances[prop] = this.spawnChancesConfig[prop];
+                }
+            }
+        }
+    }
 
-        this.setState({ collected: true, collectable: false })
-        
+    spawnParticles() {
+        this.props.particles.current.addParticle(this.element.current)
     }
 
     render() {
-        return <div className={this.composeClassName()} onClick={(e) => this.handleClick(e)} />
+        return (
+            <div 
+                ref={this.element} 
+                onAnimationEnd={(e) => this.handleAnimationEnd(e)} 
+                className={this.composeClassName()} 
+                onClick={(e) => this.onClick(e)} 
+            />
+        )
+        
     }
 }
