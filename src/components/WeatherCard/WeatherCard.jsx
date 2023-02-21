df7bddb282a3873e79a8253e52965787b1b751bc
Edoan <edoan.rodrigues@gmail.com>
2023-02-21 16:06:16 -0300

    simplify weather app and resize horizontally

--- src/components/WeatherCard/WeatherCard.jsx
+++ src/components/WeatherCard/WeatherCard.jsx
@@ -37,15 +37,15 @@ export default function WeatherCard({ data, weather, previousData, onEntityColle
 
           setTemp(Math.floor(data.main.temp))
           setPreviousTemp(previousData && previousData.main ? Math.floor(previousData.main.temp) : Math.floor(data.main.temp))
-          
+
           setLocation(`${data.name}${data.sys.country ? ', ' + data.sys.country : ''}`)
           setDescription(data.weather[0].description)
 
           const date = getDateWithTimezoneOffset((data.dt + data.timezone) * 1000)
-          
+
           const weekday = date.toLocaleString('en-US', { weekday: 'long' })
           const time = date.toLocaleString('en-US', { hourCycle: 'h23', hour: '2-digit', minute: '2-digit' })
-          
+
           setDate(`${weekday}, ${time}`)
 
           tempElementRef.current.classList.add('open')
@@ -79,31 +79,30 @@ export default function WeatherCard({ data, weather, previousData, onEntityColle
   return (
     <div className='weather-card border-anim'>
 
-      { weather && <EntitiesManager weather={weather} onEntityCollected={onEntityCollected} entities={entities} /> }
-
-      <div className='overlay column'>
-        <div className='column'>
-          <div className='top-left row'>
-            <WeatherIcon data={data} onEntityCollected={onEntityCollected} entities={entities}/>
-            <div ref={tempElementRef} className={`temp-container row loading-anim mask-hide`}>
-              <TempCounter className='temp' from={previousTemp} to={temp} />
-              <div className='metric'>°C</div>
-            </div>
+      <div className='left-area'>
+        <div className='top-left row'>
+          <WeatherIcon data={data} onEntityCollected={onEntityCollected} entities={entities} />
+          <div ref={tempElementRef} className={`temp-container row loading-anim mask-hide`}>
+            <TempCounter className='temp' from={previousTemp} to={temp} />
+            <div className='metric'>°C</div>
           </div>
-          <div className='column'>
-            <div className='info location'>
-              <span className='loading-anim close'>{location}</span>
-            </div>
-            <div className='info date'>
-              <span className='loading-anim close'>{date}</span>
-            </div>
-            <div className='info desc'>
-              <span className='loading-anim close'>{description}</span>
-            </div>
+        </div>
+        <div className='column'>
+          <div className='info location'>
+            <span className='loading-anim close'>{location}</span>
+          </div>
+          <div className='info date'>
+            <span className='loading-anim close'>{date}</span>
+          </div>
+          <div className='info desc'>
+            <span className='loading-anim close'>{description}</span>
           </div>
         </div>
       </div>
-      
+      <div className='right-area'>
+        { weather && <EntitiesManager weather={weather} onEntityCollected={onEntityCollected} entities={entities} /> }
+      </div>
+
     </div>
   )
 }
