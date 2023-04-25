1e32967ef62f8ae30064ae0e623d65744b240596
Edoan <edoan.rodrigues@gmail.com>
2023-04-25 16:01:46 -0300

    fix key icon path

--- src/components/ModalButton/index.jsx
+++ src/components/ModalButton/index.jsx
@@ -1,6 +1,6 @@
 import SmallButton from '../SmallButton';
-import icon from '../../../public/images/icons/static/key.png';
-import animatedIcon from '../../../public/images/icons/animated/key.gif';
+import icon from '/images/icons/static/key.png';
+import animatedIcon from '/images/icons/animated/key.gif';
 
 export default function ModalButton(props) {
     return <SmallButton {...props} icon={icon} animatedicon={animatedIcon} tooltip="Change API key"/>
