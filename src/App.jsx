cb2a3a6780f355360570ba98c1e34b3e81b4d578
Edoan <edoan.rodrigues@gmail.com>
2023-04-26 22:12:04 -0300

    move convertDataToWeather to the App

--- src/App.jsx
+++ src/App.jsx
@@ -14,9 +14,10 @@ import ModalButton from './components/ModalButton';
 import APIModal from './components/APIModal';
 import ReactModal from 'react-modal';
 import { Tooltip } from 'react-tooltip';
-import { converDataToWeather } from './components/EntitiesManager';
 import SmallButton from './components/SmallButton';
 import { EntitiesContext } from './contexts/entitiesContext';
+import { TEMP_TYPES } from './constants';
+import { getDateWithTimezoneOffset } from './utils';
 
 ReactModal.setAppElement('#root');
 
@@ -117,3 +118,67 @@ export default function App() {
 		</div>
 	)
 };
+
+function converDataToWeather(data) {
+    if (!data || data.loading || data.cod !== 200) return null
+
+    const temp = Math.floor(data.main.temp)
+    
+    let tempType = 0
+
+    if (temp > 32) {
+        tempType = TEMP_TYPES.verySunny
+    } else if (temp > 25) {
+        tempType = TEMP_TYPES.sunny
+    } else if (temp < 0) {
+        tempType = TEMP_TYPES.veryCold
+    } else if (temp < 15) {
+        tempType = TEMP_TYPES.cold
+    } else {
+        tempType = TEMP_TYPES.normal
+    }
+
+    const date = getDateWithTimezoneOffset((data.dt + data.timezone) * 1000)
+    const hours = date.getHours()
+    const minutes = date.getMinutes()
+
+    const time = hours * 60 + minutes // time in minutes.
+
+    // day.
+    const dayStart = 4 * 60 // from 4:00
+    const dayEnd = 18 * 60 // to 18:00
+
+    const isDay = time >= dayStart && time < dayEnd
+
+    // this is used to set the position of the moon or sun.
+    // It's a number between 0 and 1.
+    // If is sun, 0 the sun rises, 1 the sun sets, 0.5 is in the middle.
+    // Same for the moon.
+    let cycle = 0;
+
+    if (isDay) {
+        cycle = (time - dayStart) / (dayEnd - dayStart)
+    } else {
+        let t = 0;
+        if (time < dayStart) {
+            // 00:00 -> 4:00
+            t = 360 + time
+        } else {
+            // 18:00 -> 23:59
+            t = time - dayEnd;
+        }
+        
+        cycle = (t - 0) / (600 - 0)
+    }
+
+    return {
+        id: data.id,
+        temp: temp,
+        tempType,
+        time,
+        isDay,
+        cycle,
+        isRaining: Boolean(data.rain),
+        isSnowing: Boolean(data.snow)
+    }
+}
