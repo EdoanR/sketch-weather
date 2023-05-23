253f9a90f6247db9fac94ea49144ad340b0acc53
Edoan <edoan.rodrigues@gmail.com>
2023-05-23 13:55:53 -0300

    change 'api key' to 'paste api key here'

--- src/components/APIModal/index.jsx
+++ src/components/APIModal/index.jsx
@@ -32,7 +32,7 @@ export default function APIModal({ modalIsOpen, setModalIsOpen, apiKey, setApiKe
             <h2>API Key</h2>
             <p>API key is necessary to obtain weather data, you can get one by creating an account on <a href="https://home.openweathermap.org/users/sign_in" target='_blank'>OpenWeather</a>. Then going to your <a href="https://home.openweathermap.org/api_keys" target="_blank">API page</a>.</p>
             <label htmlFor="">
-                <span>API key:</span>
+                <span>Paste the API key here:</span>
                 <input type="text" onChange={handleInputChange} onKeyDown={handleKeyDown} className='border-static' placeholder='Paste the API key here...' value={apiKey} />
             </label>
         </ReactModal>
