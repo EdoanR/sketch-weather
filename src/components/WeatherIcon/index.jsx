b0cd3128c6c3c73fb0eef3509e1cbb4c1f21ff10
Edoan <edoan.rodrigues@gmail.com>
2023-05-23 13:53:00 -0300

    fix snow day entity

--- src/components/WeatherIcon/index.jsx
+++ src/components/WeatherIcon/index.jsx
@@ -40,12 +40,19 @@ export default function WeatherIcon({ data }) {
         if (clickedIcon !== currentIcon) return;
 
         const entity = getEntityFromIcon(currentIcon);
-        if (entity) collectEntity(entity);
+        if (entity) collectEntity(entity, true);
     }
 
     function getEntityFromIcon(icon) {
         if (icon === '13d') return entities.snowDay;
         return null
+    }
+
+    function getCurrentEntity() {
+        const icon = icons[selectedIconIndex];
+        if (!icon) return null;
+        const entity = getEntityFromIcon(icon);
+        return entity;
     }
 
     return (
@@ -66,19 +73,22 @@ export default function WeatherIcon({ data }) {
                 {
                     icons.map(icon => {
                         let className = 'icon'
+                        const currentEntity = getCurrentEntity();
                         const entity = getEntityFromIcon(icon)
 
-                        if (entity) className += ' entity-' + entity.keyName + ' ' + (entity.collected ? 'collected' : 'collectable');
+                        if (entity) className += ' entity ' + (entity.collected ? 'collected' : 'collectable');
 
                         return <div 
                             className={className}
                             id={icon}
                             key={icon} 
                             onClick={(e) => handleOnClick(e)}
+                            keyname={entity ? entity.keyName : null}
                             style={{ 
                                 width: iconWidth + 'px',
                                 height: iconWidth + 'px',
-                                backgroundImage: `url(${getIconUrl(icon)})`
+                                backgroundImage: `url(${entity ? entity.iconAnimated : getIconUrl(icon)})`,
+                                cursor: entity && currentEntity && entity.id === currentEntity && !entity.collected ? 'pointer' : null
                             }} 
                         />
                     })
