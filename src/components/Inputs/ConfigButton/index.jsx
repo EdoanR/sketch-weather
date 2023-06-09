7621ac9525cf082ea4233fc897a11796f24ceda7
Zennos <edoan.rodrigues@gmail.com>
2023-06-09 17:39:07 -0300

    remove config button

--- src/components/Inputs/ConfigButton/index.jsx
+++
@@ -1,9 +0,0 @@
-import './index.scss';
-
-export default function ConfigButton({ onClick }) {
-    return (
-        <div onClick={onClick} className="config-button border-anim-hover">
-            <div className="cog-icon"></div>
-        </div>
-    )
-}
