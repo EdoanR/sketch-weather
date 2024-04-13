88a90914decaa971d5fd4c9727daefc134411c37
Edoan <edoan.rodrigues@gmail.com>
2024-04-13 14:09:14 -0300

    format

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 <h1 align="center">
-  <a href="https://zennos.github.io/sketch-weather/">
+  <a href="https://edoanr.github.io/sketch-weather/">
     <picture>
       <source media="(prefers-color-scheme: dark)" srcset="https://i.imgur.com/xdqKQKV.gif">
       <source media="(prefers-color-scheme: light)" srcset="https://i.imgur.com/G3Me4Ar.gif">
@@ -16,17 +16,19 @@
 </div>
 
 ## How to use
-You can start using it by acessing this [link](https://zennos.github.io/sketch-weather/).
+
+You can start using it by acessing this [link](https://edoanr.github.io/sketch-weather/).
 
 Type a city, state, or country into search bar and see the magic!
 
 ## Local Installation
+
 Follow these steps to install and run the Sketch Weather locally:
 
 1. Clone the repository to your local machine using the following command:
 
     ```
-    git clone https://github.com/Zennos/sketch-weather.git
+    git clone https://github.com/EdoanR/sketch-weather.git
     ```
 
 2. Navigate to the project's directory:
@@ -40,7 +42,9 @@ Follow these steps to install and run the Sketch Weather locally:
     ```
     npm install
     ```
+
     or
+
     ```
     yarn install
     ```
@@ -50,13 +54,15 @@ Follow these steps to install and run the Sketch Weather locally:
     ```
     npm run dev
     ```
+
     or
+
     ```
     yarn run dev
     ```
 
-   This will launch the project in development mode, and you can access it by opening the link displayed in your console.
-   It will probably be running on `http://localhost:5173/sketch-weather/`.
+    This will launch the project in development mode, and you can access it by opening the link displayed in your console.
+    It will probably be running on `http://localhost:5173/sketch-weather/`.
 
 ## Building the project
 
@@ -65,7 +71,9 @@ To build, use the following command:
 ```
 npm run build
 ```
+
 or
+
 ```
 yarn run build
 ```
@@ -76,12 +84,15 @@ You can check if the build is working by using the following command:
 ```
 npm run preview
 ```
+
 or
+
 ```
 yarn run build
 ```
 
 ## Support me
+
 <a href='https://nubank.com.br/pagar/xldsq/rfgW3dODQh' target='_blank'><img height='35' style='border:0px;height:46px;' src='https://logospng.org/download/pix/logo-pix-icone-512.png' border='0' alt='Pix' />
 <a href='https://ko-fi.com/edoan' target='_blank'><img height='35' style='border:0px;height:46px;' src='https://az743702.vo.msecnd.net/cdn/kofi3.png?v=0' border='0' alt='Buy Me a Coffee at ko-fi.com' />
 [![paypal](https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif)](https://www.paypal.com/donate/?hosted_button_id=AP5M7UXBWR47C)
--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useRef, useState } from 'react';
-import '../node_modules/font-awesome/css/font-awesome.min.css'
-import './animations.scss'
-import './effects.scss'
-import './App.scss'
+import '../node_modules/font-awesome/css/font-awesome.min.css';
+import './animations.scss';
+import './effects.scss';
+import './App.scss';
 import SearchBar from './components/Inputs/SearchBar';
 import WeatherCard from './components/WeatherCard';
 import EntitiesList from './components/EntityList';
@@ -22,24 +22,25 @@ import { getDateWithTimezoneOffset, isDev } from './utils';
 ReactModal.setAppElement('#root');
 
 export default function App() {
-
-	const [weather, setWeather] = useState()
-	const [data, setData] = useState()
-	const [previousData, setPreviousData] = useState()
-	const [entities, setEntities] = useState(defaultEntitiesList)
+	const [weather, setWeather] = useState();
+	const [data, setData] = useState();
+	const [previousData, setPreviousData] = useState();
+	const [entities, setEntities] = useState(defaultEntitiesList);
 	const [lastCollectedEntity, setLastCollectedEntity] = useState(null);
 	const [lastUncollectedEntity, setLastUncollectedEntity] = useState(null);
-    const [apiModalIsOpen, setApiModalIsOpen] = useState(false);
-    const [resetCollectionModalIsOpen, setResetCollectionModalIsOpen] = useState(false);
-	const [apiKey, setApiKey] = useState(localStorage.getItem('api-key') || import.meta.env.VITE_OPEN_WEATHER_API_KEY || '4c6efa111e140f53a1b967afc4231d4e');
-	const [celsiusUnit, setCelsiusUnit] = useState( localStorage.getItem('celsius-unit') !== 'false' );
+	const [apiModalIsOpen, setApiModalIsOpen] = useState(false);
+	const [resetCollectionModalIsOpen, setResetCollectionModalIsOpen] = useState(false);
+	const [apiKey, setApiKey] = useState(
+		localStorage.getItem('api-key') || import.meta.env.VITE_OPEN_WEATHER_API_KEY || '4c6efa111e140f53a1b967afc4231d4e'
+	);
+	const [celsiusUnit, setCelsiusUnit] = useState(localStorage.getItem('celsius-unit') !== 'false');
 	const [searchedUsingPath, setSearchUsingPath] = useState(false);
 
-	const searchBarInputRef = useRef()
+	const searchBarInputRef = useRef();
 
 	useEffect(() => {
 		setTimeout(() => {
-			setApiModalIsOpen(!apiKey)
+			setApiModalIsOpen(!apiKey);
 		}, 1000);
 
 		const lastPathIndex = location.pathname.lastIndexOf('/');
@@ -49,51 +50,51 @@ export default function App() {
 			searchData(loc);
 			setSearchUsingPath(true);
 		}
-
-	}, [])
+	}, []);
 
 	useEffect(() => {
 		localStorage.setItem('celsius-unit', celsiusUnit);
 	}, [celsiusUnit]);
 
 	function searchData(search) {
-		if (!search) return
+		if (!search) return;
 
 		if (!apiKey) {
 			setApiModalIsOpen(true);
 			return;
 		}
 
-		setData({ loading: true })
+		setData({ loading: true });
 
-		fetch(`https://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&appid=${apiKey}`).then(async res => {
-			const data = await res.json();
+		fetch(`https://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&appid=${apiKey}`)
+			.then(async (res) => {
+				const data = await res.json();
 
-			console.log('data:', data)
+				console.log('data:', data);
 
-			setPreviousData(data)
-			setData(data)
-			const weather = converDataToWeather(data)
-			if (weather) {
-				if (isDev()) console.log('weather:', weather)
-				setWeather(weather)
-			}
-
-		}).catch(err => {
-			console.log('Error getting data:\n', err);
-			alert(`Could not get data, please check your internet connection.`);
-		})
+				setPreviousData(data);
+				setData(data);
+				const weather = converDataToWeather(data);
+				if (weather) {
+					if (isDev()) console.log('weather:', weather);
+					setWeather(weather);
+				}
+			})
+			.catch((err) => {
+				console.log('Error getting data:\n', err);
+				alert(`Could not get data, please check your internet connection.`);
+			});
 	}
 
 	function handleOnSearch(e) {
-		e.preventDefault()
+		e.preventDefault();
 
-		searchData(searchBarInputRef.current.value)
+		searchData(searchBarInputRef.current.value);
 	}
 
 	function collectEntity(entity, bubbleAnim = false, bright = false) {
-		const newEntities = {...entities};
-		newEntities[entity.keyName].collected = true
+		const newEntities = { ...entities };
+		newEntities[entity.keyName].collected = true;
 		if (bubbleAnim) newEntities[entity.keyName].bubbleAnim = true;
 		if (bright) newEntities[entity.keyName].bright = true;
 
@@ -102,7 +103,7 @@ export default function App() {
 	}
 
 	function uncollectEntity(entity) {
-		const newEntities = {...entities};
+		const newEntities = { ...entities };
 		newEntities[entity.keyName].collected = false;
 		newEntities[entity.keyName].bubbleAnim = false;
 		newEntities[entity.keyName].bright = false;
@@ -112,33 +113,51 @@ export default function App() {
 	}
 
 	function updateEntity(newEntity) {
-		const newEntities = {...entities};
+		const newEntities = { ...entities };
 		newEntities[newEntity.keyName] = newEntity;
 		setEntities(newEntities);
 	}
 
 	const entitiesContextValues = {
-		entities, lastCollectedEntity, lastUncollectedEntity,
-		collectEntity, updateEntity, uncollectEntity, setEntities
-	}
-	
+		entities,
+		lastCollectedEntity,
+		lastUncollectedEntity,
+		collectEntity,
+		updateEntity,
+		uncollectEntity,
+		setEntities,
+	};
+
 	return (
-		<div className='App'>
-			<Tooltip id='tooltip'/>
-        	<main>
-				<APIModal isOpen={apiModalIsOpen} setIsOpen={setApiModalIsOpen} apiKey={apiKey} setApiKey={setApiKey} onAfterAPISubmit={() => { if (searchBarInputRef.current.value) searchData(searchBarInputRef.current.value); }}/>
+		<div className="App">
+			<Tooltip id="tooltip" />
+			<main>
+				<APIModal
+					isOpen={apiModalIsOpen}
+					setIsOpen={setApiModalIsOpen}
+					apiKey={apiKey}
+					setApiKey={setApiKey}
+					onAfterAPISubmit={() => {
+						if (searchBarInputRef.current.value) searchData(searchBarInputRef.current.value);
+					}}
+				/>
 				<SearchBar onSubmit={handleOnSearch} searchBarInputRef={searchBarInputRef} />
 				<EntitiesContext.Provider value={entitiesContextValues}>
 					<WeatherCard weather={weather} data={data} previousData={previousData} celsiusUnit={celsiusUnit} />
 					<PreviousWeathers data={data} searchData={searchData} celsiusUnit={celsiusUnit} />
-					<EntitiesList resetModalIsOpen={setResetCollectionModalIsOpen}/>
+					<EntitiesList resetModalIsOpen={setResetCollectionModalIsOpen} />
 					<ResetCollectionModal isOpen={resetCollectionModalIsOpen} setIsOpen={setResetCollectionModalIsOpen} />
 				</EntitiesContext.Provider>
-				<div className='top-left-buttons'>
-					<FontButton className="animated" tabIndex="1"/>
-					<ModalButton className="animated" tabIndex="2" onClick={() => setApiModalIsOpen(v => !v)}/>
-					<SmallButton className="animated" tabIndex="3" onClick={() => setCelsiusUnit(v => !v)} tooltip={celsiusUnit ? 'Switch to Fahrenheit' : 'Switch to Celsius'}>
-						<div style={{fontSize: 'large', fontFamily: '"Open Sans", sans-serif'}} className='content'>
+				<div className="top-left-buttons">
+					<FontButton className="animated" tabIndex="1" />
+					<ModalButton className="animated" tabIndex="2" onClick={() => setApiModalIsOpen((v) => !v)} />
+					<SmallButton
+						className="animated"
+						tabIndex="3"
+						onClick={() => setCelsiusUnit((v) => !v)}
+						tooltip={celsiusUnit ? 'Switch to Fahrenheit' : 'Switch to Celsius'}
+					>
+						<div style={{ fontSize: 'large', fontFamily: '"Open Sans", sans-serif' }} className="content">
 							{celsiusUnit ? '°C' : '°F'}
 						</div>
 					</SmallButton>
@@ -147,103 +166,110 @@ export default function App() {
 			<footer>
 				<div>
 					<span>Made by </span>
-					<div className='signature' alt="Edoan">
+					<div className="signature" alt="Edoan">
 						<span>Edoan</span>
 					</div>
 				</div>
-				<div className='icons'>
+				<div className="icons">
 					<FooterIcon iconName="twitter" alt="Twitter" url="https://twitter.com/EuEdoan" />
-					<FooterIcon iconName="github" alt="GitHub" url="https://github.com/Zennos/sketch-weather" />
+					<FooterIcon iconName="github" alt="GitHub" url="https://github.com/EdoanR/sketch-weather" />
 					<FooterIcon iconName="ko-fi" alt="Ko-fi" url="https://ko-fi.com/edoan" />
 				</div>
 			</footer>
 		</div>
-	)
-};
+	);
+}
 
 function FooterIcon({ iconName, url, ...restProps }) {
-	const [ hovering, setHover ] = useState(false);
+	const [hovering, setHover] = useState(false);
 
 	return (
-		<a href={url} target='_blank'>
-			<img 
-				onMouseEnter={() => { setHover(true) }}
-				onMouseLeave={() => { setHover(false) }}
-				data-tooltip-id={ restProps.alt ? 'tooltip' : null} 
-				data-tooltip-content={ restProps.alt || null} 
-				src={hovering ? `/sketch-weather/images/icons/animated/${iconName}.gif` : `/sketch-weather/images/icons/static/${iconName}.png`}
+		<a href={url} target="_blank">
+			<img
+				onMouseEnter={() => {
+					setHover(true);
+				}}
+				onMouseLeave={() => {
+					setHover(false);
+				}}
+				data-tooltip-id={restProps.alt ? 'tooltip' : null}
+				data-tooltip-content={restProps.alt || null}
+				src={
+					hovering
+						? `/sketch-weather/images/icons/animated/${iconName}.gif`
+						: `/sketch-weather/images/icons/static/${iconName}.png`
+				}
 				style={{
 					width: 40,
-					height: 40
+					height: 40,
 				}}
-				{...restProps} 
+				{...restProps}
 			/>
 		</a>
-	)
-	
+	);
 }
 
 function converDataToWeather(data) {
-    if (!data || data.loading || data.cod !== 200) return null
+	if (!data || data.loading || data.cod !== 200) return null;
 
-    const temp = Math.floor(data.main.temp)
-    
-    let tempType = 0
+	const temp = Math.floor(data.main.temp);
 
-    if (temp > 32) {
-        tempType = TEMP_TYPES.verySunny
-    } else if (temp >= 25) {
-        tempType = TEMP_TYPES.sunny
-    } else if (temp < 0) {
-        tempType = TEMP_TYPES.veryCold
-    } else if (temp < 15) {
-        tempType = TEMP_TYPES.cold
-    } else {
-        tempType = TEMP_TYPES.normal
-    }
+	let tempType = 0;
 
-    const date = getDateWithTimezoneOffset((Date.now() + data.timezone) * 1000)
-    const hours = date.getHours()
-    const minutes = date.getMinutes()
+	if (temp > 32) {
+		tempType = TEMP_TYPES.verySunny;
+	} else if (temp >= 25) {
+		tempType = TEMP_TYPES.sunny;
+	} else if (temp < 0) {
+		tempType = TEMP_TYPES.veryCold;
+	} else if (temp < 15) {
+		tempType = TEMP_TYPES.cold;
+	} else {
+		tempType = TEMP_TYPES.normal;
+	}
 
-    const time = hours * 60 + minutes // time in minutes.
+	const date = getDateWithTimezoneOffset((Date.now() + data.timezone) * 1000);
+	const hours = date.getHours();
+	const minutes = date.getMinutes();
 
-    // day.
-    const dayStart = 4 * 60 // from 4:00
-    const dayEnd = 18 * 60 // to 18:00
+	const time = hours * 60 + minutes; // time in minutes.
 
-    const isDay = time >= dayStart && time < dayEnd
+	// day.
+	const dayStart = 4 * 60; // from 4:00
+	const dayEnd = 18 * 60; // to 18:00
 
-    // this is used to set the position of the moon or sun.
-    // It's a number between 0 and 1.
-    // If is sun, 0 the sun rises, 1 the sun sets, 0.5 is in the middle.
-    // Same for the moon.
-    let cycle = 0;
+	const isDay = time >= dayStart && time < dayEnd;
 
-    if (isDay) {
-        cycle = (time - dayStart) / (dayEnd - dayStart)
-    } else {
-        let t = 0;
-        if (time < dayStart) {
-            // 00:00 -> 4:00
-            t = 360 + time
-        } else {
-            // 18:00 -> 23:59
-            t = time - dayEnd;
-        }
-        
-        cycle = (t - 0) / (600 - 0)
-    }
+	// this is used to set the position of the moon or sun.
+	// It's a number between 0 and 1.
+	// If is sun, 0 the sun rises, 1 the sun sets, 0.5 is in the middle.
+	// Same for the moon.
+	let cycle = 0;
 
-    return {
-        id: data.id,
-        temp: temp,
-        tempType,
-        time,
-        isDay,
-        cycle,
-        isRaining: Boolean(data.rain),
-        isSnowing: Boolean(data.snow),
-		isExtremeTemp: tempType <= TEMP_TYPES.veryCold || tempType >= TEMP_TYPES.verySunny
-    }
+	if (isDay) {
+		cycle = (time - dayStart) / (dayEnd - dayStart);
+	} else {
+		let t = 0;
+		if (time < dayStart) {
+			// 00:00 -> 4:00
+			t = 360 + time;
+		} else {
+			// 18:00 -> 23:59
+			t = time - dayEnd;
+		}
+
+		cycle = (t - 0) / (600 - 0);
+	}
+
+	return {
+		id: data.id,
+		temp: temp,
+		tempType,
+		time,
+		isDay,
+		cycle,
+		isRaining: Boolean(data.rain),
+		isSnowing: Boolean(data.snow),
+		isExtremeTemp: tempType <= TEMP_TYPES.veryCold || tempType >= TEMP_TYPES.verySunny,
+	};
 }
